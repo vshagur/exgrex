d5c1eee67656b327b2b1fecff970649e2f662e87
vshagur <vshagur@gmail.com>
2019-09-27 10:32:40 +0700

    add exgrex/default_config.py

---
+++ exgrex/default_config.py
@@ -0,0 +1,11 @@
+DEFAULT_CONFIG = {
+    'grader_filename': 'grader.py',
+    'cwd': '/grader',
+    'cli_parameter_part_id': 'partId',
+    'env_parameter_debug': 'EXGREX_DEBUG',
+    'positive_report': 'All tests passed!',
+    'solution_filename': 'solution.py',
+    'submission_path': '/shared/submission',
+    'submission_path_debug': 'shared/submission',
+    'tests_path': 'tests/',
+}
