071eabfef2590538b7776ca0dcc4973c691889a2
vshagur <vshagur@gmail.com>
2019-09-30 14:53:53 +0700

    rename README.TXT -> README

--- setup.py
+++ setup.py
@@ -1,18 +1,25 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 from distutils.core import setup
 from setuptools import find_packages
+
+
+def readme():
+    with open('README') as f:
+        return f.read()
+
 
 setup(name='exgrex',
       version='0.1.0',
       description='The exgrex package provides the ability to create a test run ' \
                   'of various configurations. Used for external graders as an ' \
                   'entry point to the dock container.',
-
-      author='Valeriy Shagur',
+      long_description=readme(),
+      author='vshagur',
       author_email='vshagur@gmail.com',
-      # url='https://github.com/vshagur',
-      # downloadurl='https://github.com/vshagur',
-      # keywords = [‘mooc’, ‘grader’]
       packages=find_packages(exclude=['tests', '*.test.*', '*.test']),
       scripts=['bin/executeGrader', ],
+      keywords=['mooc', 'grader', 'python', 'python3'],
+      # url='https://github.com/vshagur',
+      # downloadurl='https://github.com/vshagur',
+
       )
