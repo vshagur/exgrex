e6ce8c877580fc09ea15b29120a4d11e6ddf5cb5
vshagur <vshagur@gmail.com>
2019-10-02 09:38:51 +0700

    fix import into example_grader/xxx/executor.py

--- example_grader/xxx/executor.py
+++ example_grader/xxx/executor.py
@@ -1,16 +1,9 @@
-from exgrex.actions.default_actions import check_solution_file_exist
-from exgrex.actions.default_actions import check_solution_file_name
-from exgrex.actions.default_actions import copy_solution_file
-from exgrex.actions.default_actions import add_solution_as_module
-from exgrex.actions.default_actions import run_tests
-from exgrex.actions.default_actions import format_test_result
+import exgrex.actions.base_actions as actions
 
-@check_solution_file_exist()
-@check_solution_file_name('solution.py')
-@copy_solution_file()
-@add_solution_as_module()
-@run_tests(failfast=True,traceback=True)
-@format_test_result()
+
+@actions.check_solution_file_exist()
+@actions.copy_solution_file()
+@actions.run_tests(failfast=False, traceback=False)
+@actions.format_test_result()
 def execute_grader(grader):
-    print('call execute_grader')
-    return grader.score, grader.feedback
+    return grader.feedback, grader.score
