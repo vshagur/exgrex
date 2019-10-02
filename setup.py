f083d4641f6a20ed185d786d0d708263443492cd
vshagur <vshagur@gmail.com>
2019-10-02 09:24:32 +0700

    add file executor.py

--- setup.py
+++ setup.py
@@ -2,24 +2,25 @@
 from distutils.core import setup
 from setuptools import find_packages
 
-
-def readme():
-    with open('README') as f:
-        return f.read()
-
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 
 setup(name='exgrex',
       version='0.1.0',
       description='The exgrex package provides the ability to create a test run ' \
                   'of various configurations. Used for external graders as an ' \
                   'entry point to the dock container.',
-      long_description=readme(),
+
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       author='vshagur',
       author_email='vshagur@gmail.com',
       packages=find_packages(exclude=['tests', '*.test.*', '*.test']),
       scripts=['bin/executeGrader', ],
       keywords=['mooc', 'grader', 'python', 'python3'],
       url='https://github.com/vshagur/exgrex',
-      # downloadurl='https://github.com/vshagur',
-
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: POSIX :: Linux", ],
       )
