0a9d2055dce91c63defda52d2fbf17c092b2c485
vshagur <vshagur@gmail.com>
2019-10-03 11:19:05 +0700

    add decorator into example

--- example_grader/xxx/executor.py
+++ example_grader/xxx/executor.py
@@ -1,7 +1,10 @@
 import exgrex.actions.base_actions as actions
+import exgrex.actions.extend_actions as extend_actions
 
 
 @actions.check_solution_file_exist()
+@extend_actions.check_zip()
+@extend_actions.check_files_into_zip(['submission/solution.py','gg'])
 @actions.copy_solution_file()
 @actions.run_tests(failfast=False, traceback=False)
 @actions.format_test_result()
