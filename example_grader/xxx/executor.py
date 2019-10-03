e72e378fe4df4dcc8c2845dfa497442b1ab4d5f9
vshagur <vshagur@gmail.com>
2019-10-03 09:57:30 +0700

    test commit for turn ssh connect to github

--- example_grader/xxx/executor.py
+++ example_grader/xxx/executor.py
@@ -1,8 +1,11 @@
 import exgrex.actions.base_actions as actions
-
+import exgrex.actions.extend_actions as extend_actions
 
 @actions.check_solution_file_exist()
+# @extend_actions.check_zip()
 @actions.copy_solution_file()
+# @extend_actions.glue_code(file_before_path='code_before.txt',
+#                           file_after_path='code_after.txt')
 @actions.run_tests(failfast=False, traceback=False)
 @actions.format_test_result()
 def execute_grader(grader):
