97c32c37d53431f50e09451a3d560065d67e01f0
vshagur <vshagur@gmail.com>
2019-10-03 19:06:49 +0700

    written function extract_files_from_zip

--- exgrex/actions/extend_actions.py
+++ exgrex/actions/extend_actions.py
@@ -90,14 +90,24 @@ def check_files_into_zip(filenames_list):
     return decorator
 
 
-def extract_files_from_zip(extract_parameters):
+def extract_files_from_zip(filenames):
     """
-    extract_files_from_zip
+    extract_files_from_zip, пути указаны относительные для архива и директории грейдера
     """
 
     def decorator(func):
         def wrapper(grader):
-            nonlocal extract_parameters
+            nonlocal filenames
+            zip_path = Path(grader.submission_path, grader.submission_filename)
+
+            try:
+                archive = zipfile.ZipFile(zip_path, 'r')
+                for file_name, path_to in filenames.items():
+                    archive.extract(file_name, Path(grader.grader_path, path_to))
+            except Exception as err:
+                raise GraderIOError(
+                    f'An error occurred while processing the archive with '
+                    f'the solution. Error - {err.__class__.__name__}')
 
             return func(grader)
 
@@ -109,7 +119,8 @@ def extract_files_from_zip(extract_parameters):
 def extract_all_from_zip(path_to=None):
     """
     extract_all_from_zip, может понадобиться в заданиях, на генерацию данных, когда
-    решение в виде архива потом обрабатывается тестами
+    решение в виде архива потом обрабатывается тестами, если переданный путь не
+    существует, он будет создан
     """
 
     def decorator(func):
