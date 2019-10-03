f78751ad8cb6ef02c4d3fdb1a256708d7ac5a912
vshagur <vshagur@gmail.com>
2019-10-03 19:07:43 +0700

    change xxx/executor.py

--- example_grader/xxx/executor.py
+++ example_grader/xxx/executor.py
@@ -4,9 +4,11 @@ import exgrex.actions.extend_actions as extend_actions
 
 @actions.check_solution_file_exist()
 @extend_actions.check_zip()
-@extend_actions.check_files_into_zip(['submission/solution.py','gg'])
-@actions.copy_solution_file()
-@actions.run_tests(failfast=False, traceback=False)
-@actions.format_test_result()
+@extend_actions.check_files_into_zip(['submission/solution.py'])
+@extend_actions.extract_files_from_zip({'submission/solution.py':'tests'})
+# @extend_actions.extract_all_from_zip()
+# @actions.copy_solution_file()
+# @actions.run_tests(failfast=False, traceback=False)
+# @actions.format_test_result()
 def execute_grader(grader):
     return grader.feedback, grader.score
